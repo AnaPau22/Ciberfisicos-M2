20f9d160f1fadd4da64d0ef6eb823bb286c317c1
A01657917@tec.mx <ec2-user@ip-172-31-25-220.us-east-2.compute.internal>
2022-09-29 20:00:23 +0000

    version final

--- practica_uno/ex2.py
+++ practica_uno/ex2.py
@@ -1,4 +1,32 @@
 #Programa para identificar números primos
 import sys
-print("Cuántos números primos: ")
 
+def Crear_lista(b,n):
+  b = 1
+  lista_1=[]
+  while b<n+1:
+    lista_1.append(b)
+    b+=1
+  return lista_1
+  
+def primos(lista_suma,n):
+  lista_2 = []
+  
+  for i in range(0,n):
+    contador = 0
+    for j in lista_suma:
+      if i % j == 0:
+        contador+=1
+    if contador == 2:
+      lista_2.append(i)
+
+  return lista_2
+  
+lim = sys.argv[1]
+lim = int(lim)
+
+Primera_lista= Crear_lista(1,lim)
+#print(Primera_lista)
+
+Lista_primos = primos(Primera_lista, lim)
+print(Lista_primos)
